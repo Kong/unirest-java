068f112b54ebc8965ec91cfef8651c3dce8fd12d
Mashaper <support@mashape.com>
2011-05-05 20:33:28 -0700

    boring stuff

--- src/test/java/com/mashape/client/test/http/TokenUtilTest.java
+++ src/test/java/com/mashape/client/test/http/TokenUtilTest.java
@@ -1,3 +1,27 @@
+/*
+ * 
+ * Mashape Java Client library.
+ * Copyright (C) 2011 Mashape, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ * The author of this software is Mashape, Inc.
+ * For any question or feedback please contact us at: support@mashape.com
+ * 
+ */
+
 package com.mashape.client.test.http;
 
 import static org.junit.Assert.fail;
