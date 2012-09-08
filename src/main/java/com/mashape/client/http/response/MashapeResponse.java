24144c478e36ccb8c8f3585f985e7ddae33ee6b7
thefosk <marco@mashape.com>
2012-09-07 18:48:37 -0700

    headers

--- src/main/java/com/mashape/client/http/response/MashapeResponse.java
+++ src/main/java/com/mashape/client/http/response/MashapeResponse.java
@@ -1,18 +1,24 @@
 package com.mashape.client.http.response;
 
 import java.io.InputStream;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 
 public class MashapeResponse<T> {
 	private int code;
-	private String headers;
+	private Map<String, String> headers;
 	private InputStream rawBody;
 	private T body;
 
 	public MashapeResponse(HttpResponse response, InputStream rawBody, T body) {
-		this.headers = Arrays.toString(response.getAllHeaders());
+		Header[] allHeaders = response.getAllHeaders();
+		this.headers = new HashMap<String, String>();
+		for(Header header : allHeaders) {
+			headers.put(header.getName(), header.getValue());
+		}
 		this.code = response.getStatusLine().getStatusCode();
 		this.rawBody = rawBody;
 		this.body = body;
@@ -21,7 +27,7 @@ public class MashapeResponse<T> {
 	public int getCode() {
 		return code;
 	}
-	public String getHeaders() {
+	public Map<String, String> getHeaders() {
 		return headers;
 	}
 	public InputStream getRawBody() {
