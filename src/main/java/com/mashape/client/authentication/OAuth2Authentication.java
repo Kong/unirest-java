e547e968512b17d64781ab8f8afe4b8fe33cf701
thefosk <marco@mashape.com>
2012-10-11 15:22:06 -0700

    refacotr

--- src/main/java/com/mashape/client/authentication/OAuth2Authentication.java
+++ src/main/java/com/mashape/client/authentication/OAuth2Authentication.java
@@ -3,8 +3,8 @@ package com.mashape.client.authentication;
 public class OAuth2Authentication extends OAuthAuthentication {
 
 	public OAuth2Authentication(String consumerKey, String consumerSecret,
-			String redirectUrl) {
-		super(consumerKey, consumerSecret, redirectUrl);
+			String callbackUrl) {
+		super(consumerKey, consumerSecret, callbackUrl);
 	}
 
 }
--- src/main/java/com/mashape/client/authentication/OAuthAuthentication.java
+++ src/main/java/com/mashape/client/authentication/OAuthAuthentication.java
@@ -2,16 +2,16 @@ package com.mashape.client.authentication;
 
 public abstract class OAuthAuthentication extends Authentication {
 
-	public static final String REDIRECT_URL = "redirectUrl";
+	public static final String CALLBACK_URL = "callbackUrl";
 	public static final String CONSUMER_SECRET = "consumerSecret";
 	public static final String CONSUMER_KEY = "consumerKey";
 	public static final String ACCESS_TOKEN = "accessToken";
 	public static final String ACCESS_SECRET = "accessSecret";
 
-	protected OAuthAuthentication(String consumerKey, String consumerSecret, String redirectUrl) {
+	protected OAuthAuthentication(String consumerKey, String consumerSecret, String callbackUrl) {
 		queryParameters.put(CONSUMER_KEY, consumerKey);
 		queryParameters.put(CONSUMER_SECRET, consumerSecret);
-		queryParameters.put(REDIRECT_URL, redirectUrl);
+		queryParameters.put(CALLBACK_URL, callbackUrl);
 	}
 	
 	public void setAccessToken(String accessToken) {
