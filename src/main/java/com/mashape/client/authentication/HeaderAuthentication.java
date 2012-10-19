62c69db8c893d0e4fdd0379d6bc71c55f5ccee51
thefosk <marco@mashape.com>
2012-10-18 17:10:23 -0700

    header auth fix

--- src/main/java/com/mashape/client/authentication/HeaderAuthentication.java
+++ src/main/java/com/mashape/client/authentication/HeaderAuthentication.java
@@ -4,7 +4,7 @@ import org.apache.http.message.BasicHeader;
 
 public class HeaderAuthentication extends Authentication {
 	
-	public void addHeader(AuthenticationParameter ... headers) {
+	public HeaderAuthentication(AuthenticationParameter ... headers) {
 		for (AuthenticationParameter header : headers) {
 			super.headers.add(new BasicHeader(header.getName(), header.getValue()));
 		}
