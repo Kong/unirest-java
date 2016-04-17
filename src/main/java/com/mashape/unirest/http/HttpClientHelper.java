d082d0e78d563885ac6becbce4ed20fb4c31235b
bennyl <benny.lutati@gmail.com>
2016-04-17 20:28:51 +0300

    updating some of the methods to use CompletableFuture instead of Future, also upgrade the project to use java8.

--- src/main/java/com/mashape/unirest/http/HttpClientHelper.java
+++ src/main/java/com/mashape/unirest/http/HttpClientHelper.java
@@ -22,7 +22,6 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 package com.mashape.unirest.http;
 
 import java.io.ByteArrayOutputStream;
@@ -60,190 +59,224 @@ import com.mashape.unirest.http.options.Option;
 import com.mashape.unirest.http.options.Options;
 import com.mashape.unirest.http.utils.ClientFactory;
 import com.mashape.unirest.request.HttpRequest;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
 
 public class HttpClientHelper {
 
-	private static final String CONTENT_TYPE = "content-type";
-	private static final String ACCEPT_ENCODING_HEADER = "accept-encoding";
-	private static final String USER_AGENT_HEADER = "user-agent";
-	private static final String USER_AGENT = "unirest-java/1.3.11";
+    private static final String CONTENT_TYPE = "content-type";
+    private static final String ACCEPT_ENCODING_HEADER = "accept-encoding";
+    private static final String USER_AGENT_HEADER = "user-agent";
+    private static final String USER_AGENT = "unirest-java/1.3.11";
 
-	private static <T> FutureCallback<org.apache.http.HttpResponse> prepareCallback(final Class<T> responseClass, final Callback<T> callback) {
-		if (callback == null)
-			return null;
+    private static <T> FutureCallback<org.apache.http.HttpResponse> prepareCallback(final Class<T> responseClass, final Callback<T> callback) {
+        if (callback == null) {
+            return null;
+        }
 
-		return new FutureCallback<org.apache.http.HttpResponse>() {
+        return new FutureCallback<org.apache.http.HttpResponse>() {
 
-			public void cancelled() {
-				callback.cancelled();
-			}
+            public void cancelled() {
+                callback.cancelled();
+            }
 
-			public void completed(org.apache.http.HttpResponse arg0) {
-				callback.completed(new HttpResponse<T>(arg0, responseClass));
-			}
+            public void completed(org.apache.http.HttpResponse arg0) {
+                callback.completed(new HttpResponse<T>(arg0, responseClass));
+            }
 
-			public void failed(Exception arg0) {
-				callback.failed(new UnirestException(arg0));
-			}
+            public void failed(Exception arg0) {
+                callback.failed(new UnirestException(arg0));
+            }
 
-		};
-	}
+        };
+    }
 
-	public static <T> Future<HttpResponse<T>> requestAsync(HttpRequest request, final Class<T> responseClass, Callback<T> callback) {
-		HttpUriRequest requestObj = prepareRequest(request, true);
+    public static <T> CompletableFuture<HttpResponse<T>> requestAsync(HttpRequest request, final Class<T> responseClass) {
+        CompletableFuture<HttpResponse<T>> result = new CompletableFuture<HttpResponse<T>>();
+        Future<HttpResponse<T>> future = requestAsync(request, responseClass, new Callback<T>() {
+            public void completed(HttpResponse<T> response) {
+                result.complete(response);
+            }
 
-		CloseableHttpAsyncClient asyncHttpClient = ClientFactory.getAsyncHttpClient();
-                AsyncIdleConnectionMonitorThread asyncIdleConnectionMonitorThread = (AsyncIdleConnectionMonitorThread) Options.getOption(Option.ASYNC_MONITOR);
-                if (!asyncIdleConnectionMonitorThread.isAlive()) {
-                    synchronized (asyncIdleConnectionMonitorThread) {
-                        if (!asyncIdleConnectionMonitorThread.isAlive()) {
-                                asyncHttpClient.start();
-                                asyncIdleConnectionMonitorThread.start();
-                        }
-                    }
+            public void failed(UnirestException e) {
+                result.completeExceptionally(e);
+            }
+
+            public void cancelled() {
+                if (!result.isCancelled()) {
+                    result.cancel(true); //this should also be reported? how can this happen??
                 }
+            }
+        });
 
-		final Future<org.apache.http.HttpResponse> future = asyncHttpClient.execute(requestObj, prepareCallback(responseClass, callback));
+        result.exceptionally(ex -> {
+            if (ex instanceof CancellationException) {
+                future.cancel(true);
+            }
+            return null;
+        });
 
-		return new Future<HttpResponse<T>>() {
+        return result;
 
-			public boolean cancel(boolean mayInterruptIfRunning) {
-				return future.cancel(mayInterruptIfRunning);
-			}
+    }
 
-			public boolean isCancelled() {
-				return future.isCancelled();
-			}
+    public static <T> Future<HttpResponse<T>> requestAsync(HttpRequest request, final Class<T> responseClass, Callback<T> callback) {
+        HttpUriRequest requestObj = prepareRequest(request, true);
 
-			public boolean isDone() {
-				return future.isDone();
-			}
+        CloseableHttpAsyncClient asyncHttpClient = ClientFactory.getAsyncHttpClient();
+        AsyncIdleConnectionMonitorThread asyncIdleConnectionMonitorThread = (AsyncIdleConnectionMonitorThread) Options.getOption(Option.ASYNC_MONITOR);
+        if (!asyncIdleConnectionMonitorThread.isAlive()) {
+            synchronized (asyncIdleConnectionMonitorThread) {
+                if (!asyncIdleConnectionMonitorThread.isAlive()) {
+                    asyncHttpClient.start();
+                    asyncIdleConnectionMonitorThread.start();
+                }
+            }
+        }
 
-			public HttpResponse<T> get() throws InterruptedException, ExecutionException {
-				org.apache.http.HttpResponse httpResponse = future.get();
-				return new HttpResponse<T>(httpResponse, responseClass);
-			}
+        final Future<org.apache.http.HttpResponse> future = asyncHttpClient.execute(requestObj, prepareCallback(responseClass, callback));
 
-			public HttpResponse<T> get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-				org.apache.http.HttpResponse httpResponse = future.get(timeout, unit);
-				return new HttpResponse<T>(httpResponse, responseClass);
-			}
-		};
-	}
+        return new Future<HttpResponse<T>>() {
 
-	public static <T> HttpResponse<T> request(HttpRequest request, Class<T> responseClass) throws UnirestException {
-		HttpRequestBase requestObj = prepareRequest(request, false);
-		HttpClient client = ClientFactory.getHttpClient(); // The
-															// DefaultHttpClient
-															// is thread-safe
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                return future.cancel(mayInterruptIfRunning);
+            }
 
-		org.apache.http.HttpResponse response;
-		try {
-			response = client.execute(requestObj);
-			HttpResponse<T> httpResponse = new HttpResponse<T>(response, responseClass);
-			requestObj.releaseConnection();
-			return httpResponse;
-		} catch (Exception e) {
-			throw new UnirestException(e);
-		} finally {
-			requestObj.releaseConnection();
-		}
-	}
+            public boolean isCancelled() {
+                return future.isCancelled();
+            }
 
-	private static HttpRequestBase prepareRequest(HttpRequest request, boolean async) {
+            public boolean isDone() {
+                return future.isDone();
+            }
 
-		Object defaultHeaders = Options.getOption(Option.DEFAULT_HEADERS);
-		if (defaultHeaders != null) {
-			@SuppressWarnings("unchecked")
-			Set<Entry<String, String>> entrySet = ((Map<String, String>) defaultHeaders).entrySet();
-			for (Entry<String, String> entry : entrySet) {
-				request.header(entry.getKey(), entry.getValue());
-			}
-		}
+            public HttpResponse<T> get() throws InterruptedException, ExecutionException {
+                org.apache.http.HttpResponse httpResponse = future.get();
+                return new HttpResponse<T>(httpResponse, responseClass);
+            }
 
-		if (!request.getHeaders().containsKey(USER_AGENT_HEADER)) {
-			request.header(USER_AGENT_HEADER, USER_AGENT);
-		}
-		if (!request.getHeaders().containsKey(ACCEPT_ENCODING_HEADER)) {
-			request.header(ACCEPT_ENCODING_HEADER, "gzip");
-		}
+            public HttpResponse<T> get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+                org.apache.http.HttpResponse httpResponse = future.get(timeout, unit);
+                return new HttpResponse<T>(httpResponse, responseClass);
+            }
+        };
+    }
 
-		HttpRequestBase reqObj = null;
+    public static <T> HttpResponse<T> request(HttpRequest request, Class<T> responseClass) throws UnirestException {
+        HttpRequestBase requestObj = prepareRequest(request, false);
+        HttpClient client = ClientFactory.getHttpClient(); // The
+        // DefaultHttpClient
+        // is thread-safe
 
-		String urlToRequest = null;
-		try {
-			URL url = new URL(request.getUrl());
-			URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), URLDecoder.decode(url.getPath(), "UTF-8"), "", url.getRef());
-			urlToRequest = uri.toURL().toString();
-			if (url.getQuery() != null && !url.getQuery().trim().equals("")) {
-				if (!urlToRequest.substring(urlToRequest.length() - 1).equals("?")) {
-					urlToRequest += "?";
-				}
-				urlToRequest += url.getQuery();
-			} else if (urlToRequest.substring(urlToRequest.length() - 1).equals("?")) {
-				urlToRequest = urlToRequest.substring(0, urlToRequest.length() - 1);
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+        org.apache.http.HttpResponse response;
+        try {
+            response = client.execute(requestObj);
+            HttpResponse<T> httpResponse = new HttpResponse<T>(response, responseClass);
+            requestObj.releaseConnection();
+            return httpResponse;
+        } catch (Exception e) {
+            throw new UnirestException(e);
+        } finally {
+            requestObj.releaseConnection();
+        }
+    }
 
-		switch (request.getHttpMethod()) {
-		case GET:
-			reqObj = new HttpGet(urlToRequest);
-			break;
-		case POST:
-			reqObj = new HttpPost(urlToRequest);
-			break;
-		case PUT:
-			reqObj = new HttpPut(urlToRequest);
-			break;
-		case DELETE:
-			reqObj = new HttpDeleteWithBody(urlToRequest);
-			break;
-		case PATCH:
-			reqObj = new HttpPatchWithBody(urlToRequest);
-			break;
-		case OPTIONS:
-			reqObj = new HttpOptions(urlToRequest);
-			break;
-		case HEAD:
-			reqObj = new HttpHead(urlToRequest);
-			break;
-		}
+    private static HttpRequestBase prepareRequest(HttpRequest request, boolean async) {
 
-		Set<Entry<String, List<String>>> entrySet = request.getHeaders().entrySet();
-		for (Entry<String, List<String>> entry : entrySet) {
-			List<String> values = entry.getValue();
-			if (values != null) {
-				for (String value : values) {
-					reqObj.addHeader(entry.getKey(), value);
-				}
-			}
-		}
+        Object defaultHeaders = Options.getOption(Option.DEFAULT_HEADERS);
+        if (defaultHeaders != null) {
+            @SuppressWarnings("unchecked")
+            Set<Entry<String, String>> entrySet = ((Map<String, String>) defaultHeaders).entrySet();
+            for (Entry<String, String> entry : entrySet) {
+                request.header(entry.getKey(), entry.getValue());
+            }
+        }
 
-		// Set body
-		if (!(request.getHttpMethod() == HttpMethod.GET || request.getHttpMethod() == HttpMethod.HEAD)) {
-			if (request.getBody() != null) {
-				HttpEntity entity = request.getBody().getEntity();
-				if (async) {
-					if (reqObj.getHeaders(CONTENT_TYPE) == null || reqObj.getHeaders(CONTENT_TYPE).length == 0) {
-						reqObj.setHeader(entity.getContentType());
-					}
-					try {
-						ByteArrayOutputStream output = new ByteArrayOutputStream();
-						entity.writeTo(output);
-						NByteArrayEntity en = new NByteArrayEntity(output.toByteArray());
-						((HttpEntityEnclosingRequestBase) reqObj).setEntity(en);
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				} else {
-					((HttpEntityEnclosingRequestBase) reqObj).setEntity(entity);
-				}
-			}
-		}
+        if (!request.getHeaders().containsKey(USER_AGENT_HEADER)) {
+            request.header(USER_AGENT_HEADER, USER_AGENT);
+        }
+        if (!request.getHeaders().containsKey(ACCEPT_ENCODING_HEADER)) {
+            request.header(ACCEPT_ENCODING_HEADER, "gzip");
+        }
 
-		return reqObj;
-	}
+        HttpRequestBase reqObj = null;
+
+        String urlToRequest = null;
+        try {
+            URL url = new URL(request.getUrl());
+            URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), URLDecoder.decode(url.getPath(), "UTF-8"), "", url.getRef());
+            urlToRequest = uri.toURL().toString();
+            if (url.getQuery() != null && !url.getQuery().trim().equals("")) {
+                if (!urlToRequest.substring(urlToRequest.length() - 1).equals("?")) {
+                    urlToRequest += "?";
+                }
+                urlToRequest += url.getQuery();
+            } else if (urlToRequest.substring(urlToRequest.length() - 1).equals("?")) {
+                urlToRequest = urlToRequest.substring(0, urlToRequest.length() - 1);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        switch (request.getHttpMethod()) {
+            case GET:
+                reqObj = new HttpGet(urlToRequest);
+                break;
+            case POST:
+                reqObj = new HttpPost(urlToRequest);
+                break;
+            case PUT:
+                reqObj = new HttpPut(urlToRequest);
+                break;
+            case DELETE:
+                reqObj = new HttpDeleteWithBody(urlToRequest);
+                break;
+            case PATCH:
+                reqObj = new HttpPatchWithBody(urlToRequest);
+                break;
+            case OPTIONS:
+                reqObj = new HttpOptions(urlToRequest);
+                break;
+            case HEAD:
+                reqObj = new HttpHead(urlToRequest);
+                break;
+        }
+
+        Set<Entry<String, List<String>>> entrySet = request.getHeaders().entrySet();
+        for (Entry<String, List<String>> entry : entrySet) {
+            List<String> values = entry.getValue();
+            if (values != null) {
+                for (String value : values) {
+                    reqObj.addHeader(entry.getKey(), value);
+                }
+            }
+        }
+
+        // Set body
+        if (!(request.getHttpMethod() == HttpMethod.GET || request.getHttpMethod() == HttpMethod.HEAD)) {
+            if (request.getBody() != null) {
+                HttpEntity entity = request.getBody().getEntity();
+                if (async) {
+                    if (reqObj.getHeaders(CONTENT_TYPE) == null || reqObj.getHeaders(CONTENT_TYPE).length == 0) {
+                        reqObj.setHeader(entity.getContentType());
+                    }
+                    try {
+                        ByteArrayOutputStream output = new ByteArrayOutputStream();
+                        entity.writeTo(output);
+                        NByteArrayEntity en = new NByteArrayEntity(output.toByteArray());
+                        ((HttpEntityEnclosingRequestBase) reqObj).setEntity(en);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    ((HttpEntityEnclosingRequestBase) reqObj).setEntity(entity);
+                }
+            }
+        }
+
+        return reqObj;
+    }
 
 }
--- src/main/java/com/mashape/unirest/request/BaseRequest.java
+++ src/main/java/com/mashape/unirest/request/BaseRequest.java
@@ -22,7 +22,6 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 package com.mashape.unirest.request;
 
 import com.mashape.unirest.http.HttpClientHelper;
@@ -32,72 +31,75 @@ import com.mashape.unirest.http.async.Callback;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
+import java.util.function.BiFunction;
 
 public abstract class BaseRequest {
 
-	protected static final String UTF_8 = "UTF-8";
+    protected static final String UTF_8 = "UTF-8";
 
-	protected HttpRequest httpRequest;
+    protected HttpRequest httpRequest;
 
-	protected BaseRequest(HttpRequest httpRequest) {
-		this.httpRequest = httpRequest;
-	}
+    protected BaseRequest(HttpRequest httpRequest) {
+        this.httpRequest = httpRequest;
+    }
 
-	public HttpRequest getHttpRequest() {
-		return this.httpRequest;
-	}
+    public HttpRequest getHttpRequest() {
+        return this.httpRequest;
+    }
 
-	protected BaseRequest() {
-		super();
-	}
+    protected BaseRequest() {
+        super();
+    }
 
-	public HttpResponse<String> asString() throws UnirestException {
-		return HttpClientHelper.request(httpRequest, String.class);
-	}
+    public HttpResponse<String> asString() throws UnirestException {
+        return HttpClientHelper.request(httpRequest, String.class);
+    }
 
-	public Future<HttpResponse<String>> asStringAsync() {
-		return HttpClientHelper.requestAsync(httpRequest, String.class, null);
-	}
+    public CompletableFuture<HttpResponse<String>> asStringAsync() {
+        return HttpClientHelper.requestAsync(httpRequest, String.class);
+    }
 
-	public Future<HttpResponse<String>> asStringAsync(Callback<String> callback) {
-		return HttpClientHelper.requestAsync(httpRequest, String.class, callback);
-	}
+    public Future<HttpResponse<String>> asStringAsync(Callback<String> callback) {
+        return HttpClientHelper.requestAsync(httpRequest, String.class, callback);
+    }
 
-	public HttpResponse<JsonNode> asJson() throws UnirestException {
-		return HttpClientHelper.request(httpRequest, JsonNode.class);
-	}
+    public HttpResponse<JsonNode> asJson() throws UnirestException {
+        return HttpClientHelper.request(httpRequest, JsonNode.class);
+    }
 
-	public Future<HttpResponse<JsonNode>> asJsonAsync() {
-		return HttpClientHelper.requestAsync(httpRequest, JsonNode.class, null);
-	}
+    public CompletableFuture<HttpResponse<JsonNode>> asJsonAsync() {
+        return HttpClientHelper.requestAsync(httpRequest, JsonNode.class);
+    }
 
-	public Future<HttpResponse<JsonNode>> asJsonAsync(Callback<JsonNode> callback) {
-		return HttpClientHelper.requestAsync(httpRequest, JsonNode.class, callback);
-	}
+            
+    public Future<HttpResponse<JsonNode>> asJsonAsync(Callback<JsonNode> callback) {
+        return HttpClientHelper.requestAsync(httpRequest, JsonNode.class, callback);
+    }
 
-	public <T> HttpResponse<T> asObject(Class<? extends T> responseClass) throws UnirestException {
-		return HttpClientHelper.request(httpRequest, (Class) responseClass);
-	}
+    public <T> HttpResponse<T> asObject(Class<? extends T> responseClass) throws UnirestException {
+        return HttpClientHelper.request(httpRequest, (Class) responseClass);
+    }
 
-	public <T> Future<HttpResponse<T>> asObjectAsync(Class<? extends T> responseClass) {
-		return HttpClientHelper.requestAsync(httpRequest, (Class) responseClass, null);
-	}
+    public <T> CompletableFuture<HttpResponse<T>> asObjectAsync(Class<? extends T> responseClass) {
+        return HttpClientHelper.requestAsync(httpRequest, (Class) responseClass);
+    }
 
-	public <T> Future<HttpResponse<T>> asObjectAsync(Class<? extends T> responseClass, Callback<T> callback) {
-		return HttpClientHelper.requestAsync(httpRequest, (Class) responseClass, callback);
-	}
+    public <T> Future<HttpResponse<T>> asObjectAsync(Class<? extends T> responseClass, Callback<T> callback) {
+        return HttpClientHelper.requestAsync(httpRequest, (Class) responseClass, callback);
+    }
 
-	public HttpResponse<InputStream> asBinary() throws UnirestException {
-		return HttpClientHelper.request(httpRequest, InputStream.class);
-	}
+    public HttpResponse<InputStream> asBinary() throws UnirestException {
+        return HttpClientHelper.request(httpRequest, InputStream.class);
+    }
 
-	public Future<HttpResponse<InputStream>> asBinaryAsync() {
-		return HttpClientHelper.requestAsync(httpRequest, InputStream.class, null);
-	}
+    public CompletableFuture<HttpResponse<InputStream>> asBinaryAsync() {
+        return HttpClientHelper.requestAsync(httpRequest, InputStream.class);
+    }
 
-	public Future<HttpResponse<InputStream>> asBinaryAsync(Callback<InputStream> callback) {
-		return HttpClientHelper.requestAsync(httpRequest, InputStream.class, callback);
-	}
+    public Future<HttpResponse<InputStream>> asBinaryAsync(Callback<InputStream> callback) {
+        return HttpClientHelper.requestAsync(httpRequest, InputStream.class, callback);
+    }
 
 }
