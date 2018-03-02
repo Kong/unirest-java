0f8fdaa88957a41e39fbc676d9975872502101b7
Ryan Bergman <ryan.bergman@gmail.com>
2018-03-02 13:36:01 -0600

    Unirest exception should not be checked. Fixes #5

--- src/main/java/com/mashape/unirest/http/exceptions/UnirestException.java
+++ src/main/java/com/mashape/unirest/http/exceptions/UnirestException.java
@@ -1,6 +1,6 @@
 package com.mashape.unirest.http.exceptions;
 
-public class UnirestException extends Exception {
+public class UnirestException extends RuntimeException {
 
 	private static final long serialVersionUID = -3714840499934575734L;
 
