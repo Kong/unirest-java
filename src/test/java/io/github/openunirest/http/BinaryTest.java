a13cb4a064c1295d854906d6f3903b23504290df
Ryan Bergman <ryan.bergman@gmail.com>
2018-03-23 19:34:18 -0500

    binary test without callback

--- src/test/java/io/github/openunirest/http/BinaryTest.java
+++ src/test/java/io/github/openunirest/http/BinaryTest.java
@@ -4,21 +4,32 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class BinaryTest extends BddTest {
     @Test
-    public void canGetBinaryResponse() throws IOException {
+    public void canGetBinaryResponse() {
         HttpResponse<InputStream> i = Unirest.get(MockServer.GET)
                 .queryString("foo", "bar")
                 .asBinary();
 
         RequestCapture cap = TestUtil.readValue(i.getBody(), RequestCapture.class);
-
         cap.assertParam("foo", "bar");
     }
 
     @Test
-    public void canGetBinaryResponseAsync() {
+    public void canGetBinaryResponseAsync() throws ExecutionException, InterruptedException {
+        CompletableFuture<HttpResponse<InputStream>> r = Unirest.get(MockServer.GET)
+                .queryString("foo", "bar")
+                .asBinaryAsync();
+
+        RequestCapture cap = TestUtil.readValue(r.get().getBody(), RequestCapture.class);
+        cap.assertParam("foo", "bar");
+    }
+
+    @Test
+    public void canGetBinaryResponseAsyncWithCallback() {
         Unirest.get(MockServer.GET)
                 .queryString("foo", "bar")
                 .asBinaryAsync(r -> {
