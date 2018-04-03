d6de85f32024a224c1d788105b2fe5491d8dec42
Ryan Bergman <ryan.bergman@gmail.com>
2018-04-02 19:46:05 -0500

    small java-8 refactoring

--- src/main/java/io/github/openunirest/http/Headers.java
+++ src/main/java/io/github/openunirest/http/Headers.java
@@ -4,33 +4,33 @@ import org.apache.http.Header;
 import org.omg.CORBA.NameValuePair;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class Headers extends HashMap<String, List<String>> {
 
-	private static final long serialVersionUID = 71310341388734766L;
+    private static final long serialVersionUID = 71310341388734766L;
 
-	public Headers(){
-		super();
-	}
+    public Headers() {
+        super();
+    }
 
-	public Headers(Header[] pairs){
-		for (Header header : pairs) {
-			add(header.getName(), header.getValue());
-		}
-	}
+    public Headers(Header[] pairs) {
+        for (Header header : pairs) {
+            add(header.getName(), header.getValue());
+        }
+    }
 
-	public String getFirst(Object key) {
-		List<String> list = get(key);
-		if (list != null && list.size() > 0) {
-			return list.get(0);
-		}
-		return null;
-	}
+    public String getFirst(Object key) {
+        return getOrDefault(key, Collections.emptyList())
+                .stream()
+                .findFirst()
+                .orElse(null);
+    }
 
-	public void add(String name, String value){
-		computeIfAbsent(name, k -> new ArrayList<>()).add(value);
-	}
+    public void add(String name, String value) {
+        computeIfAbsent(name, k -> new ArrayList<>()).add(value);
+    }
 
 }
