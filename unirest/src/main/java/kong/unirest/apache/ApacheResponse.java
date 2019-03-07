556ccfbc1b6ce9021b370cd9ebec28fecce51462
Ryan Bergman <ryan.bergman@gmail.com>
2019-03-07 06:41:25 -0600

    closes #190 make sure the GZIPInputStream gets closed

--- unirest/src/main/java/kong/unirest/apache/ApacheResponse.java
+++ unirest/src/main/java/kong/unirest/apache/ApacheResponse.java
@@ -38,23 +38,23 @@ import java.util.zip.GZIPInputStream;
 class ApacheResponse extends RawResponseBase {
     private final HttpResponse r;
 
-    public ApacheResponse(HttpResponse r, Config config){
+    public ApacheResponse(HttpResponse r, Config config) {
         super(config);
         this.r = r;
     }
 
     @Override
-    public int getStatus(){
+    public int getStatus() {
         return r.getStatusLine().getStatusCode();
     }
 
     @Override
-    public String getStatusText(){
+    public String getStatusText() {
         return r.getStatusLine().getReasonPhrase();
     }
 
     @Override
-    public Headers getHeaders(){
+    public Headers getHeaders() {
         Headers h = new Headers();
         Stream.of(r.getAllHeaders())
                 .forEachOrdered(e -> h.add(e.getName(), e.getValue()));
@@ -62,10 +62,10 @@ class ApacheResponse extends RawResponseBase {
     }
 
     @Override
-    public InputStream getContent(){
+    public InputStream getContent() {
         try {
             HttpEntity entity = r.getEntity();
-            if(entity != null) {
+            if (entity != null) {
                 return entity.getContent();
             }
             return new ByteArrayInputStream(new byte[0]);
@@ -76,7 +76,7 @@ class ApacheResponse extends RawResponseBase {
 
     @Override
     public byte[] getContentAsBytes() {
-        if(!hasContent()){
+        if (!hasContent()) {
             return new byte[0];
         }
         try {
@@ -99,7 +99,7 @@ class ApacheResponse extends RawResponseBase {
 
     @Override
     public String getContentAsString(String charset) {
-        if(!hasContent()){
+        if (!hasContent()) {
             return "";
         }
         try {
@@ -111,14 +111,14 @@ class ApacheResponse extends RawResponseBase {
     }
 
     private String getCharset(String charset) {
-        if(charset == null || charset.trim().isEmpty()){
+        if (charset == null || charset.trim().isEmpty()) {
             return getCharSet();
         }
         return charset;
     }
 
     @Override
-    public InputStreamReader getContentReader(){
+    public InputStreamReader getContentReader() {
         return new InputStreamReader(getContent());
     }
 
@@ -129,9 +129,9 @@ class ApacheResponse extends RawResponseBase {
 
     @Override
     public String getContentType() {
-        if(hasContent()){
+        if (hasContent()) {
             Header contentType = r.getEntity().getContentType();
-            if(contentType != null){
+            if (contentType != null) {
                 return contentType.getValue();
             }
         }
@@ -140,9 +140,9 @@ class ApacheResponse extends RawResponseBase {
 
     @Override
     public String getEncoding() {
-        if(hasContent()){
+        if (hasContent()) {
             Header contentType = r.getEntity().getContentEncoding();
-            if(contentType != null){
+            if (contentType != null) {
                 return contentType.getValue();
             }
         }
@@ -150,23 +150,27 @@ class ApacheResponse extends RawResponseBase {
     }
 
     private static byte[] getBytes(InputStream is) throws IOException {
-        int len;
-        int size = 1024;
-        byte[] buf;
+        try {
+            int len;
+            int size = 1024;
+            byte[] buf;
 
-        if (is instanceof ByteArrayInputStream) {
-            size = is.available();
-            buf = new byte[size];
-            len = is.read(buf, 0, size);
-        } else {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            buf = new byte[size];
-            while ((len = is.read(buf, 0, size)) != -1) {
-                bos.write(buf, 0, len);
+            if (is instanceof ByteArrayInputStream) {
+                size = is.available();
+                buf = new byte[size];
+                len = is.read(buf, 0, size);
+            } else {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                buf = new byte[size];
+                while ((len = is.read(buf, 0, size)) != -1) {
+                    bos.write(buf, 0, len);
+                }
+                buf = bos.toByteArray();
             }
-            buf = bos.toByteArray();
+            return buf;
+        } finally {
+            is.close();
         }
-        return buf;
     }
 
     private static boolean isGzipped(String value) {
