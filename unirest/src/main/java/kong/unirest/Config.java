72ba2b0651d319a4a3bf37c020b0b6905ede75f2
Ryan Bergman <bergmanpatrickr@johndeere.com>
2019-05-06 08:53:23 -0500

    keep the keystore password as a lambda which is slightly more secure than having a cleartext password

--- unirest/src/main/java/kong/unirest/Config.java
+++ unirest/src/main/java/kong/unirest/Config.java
@@ -1,8 +1,8 @@
 /**
  * The MIT License
- * <p>
+ *
  * Copyright for portions of unirest-java are held by Kong Inc (c) 2013.
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -10,10 +10,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -73,7 +73,7 @@ public class Config {
     private boolean verifySsl = true;
     private boolean addShutdownHook = false;
     private KeyStore keystore;
-    private String keystorePassword;
+    private Supplier<String> keystorePassword = () -> null;
     private String cookieSpec;
     private UniMetric metrics = new NoopMetric();
 
@@ -220,7 +220,7 @@ public class Config {
      */
     public Config clientCertificateStore(KeyStore store, String password) {
         this.keystore = store;
-        this.keystorePassword = password;
+        this.keystorePassword = () -> password;
         return this;
     }
 
@@ -233,9 +233,9 @@ public class Config {
      */
     public Config clientCertificateStore(String fileLocation, String password) {
         try (InputStream keyStoreStream = Util.getFileInputStream(fileLocation)) {
-            this.keystorePassword = password;
+            this.keystorePassword = () -> password;
             this.keystore = KeyStore.getInstance("PKCS12");
-            this.keystore.load(keyStoreStream, keystorePassword.toCharArray());
+            this.keystore.load(keyStoreStream, keystorePassword.get().toCharArray());
         } catch (Exception e) {
             throw new UnirestConfigException(e);
         }
@@ -643,7 +643,7 @@ public class Config {
      * @return The password for the keystore if provided
      */
     public String getKeyStorePassword() {
-        return this.keystorePassword;
+        return this.keystorePassword.get();
     }
 
     /**
