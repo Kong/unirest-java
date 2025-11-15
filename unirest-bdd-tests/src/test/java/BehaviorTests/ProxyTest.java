cbd2dc6ecbbe2ac56923a3dfe34b7eb26f38e7bb
Ryan Bergman <Ryan.Bergman@gmail.com>
2025-11-15 07:48:15 -0600

    format

--- unirest-bdd-tests/src/test/java/BehaviorTests/ProxyTest.java
+++ unirest-bdd-tests/src/test/java/BehaviorTests/ProxyTest.java
@@ -1,8 +1,8 @@
 /**
  * The MIT License
- * <p>
+ *
  * Copyright for portions of unirest-java are held by Kong Inc (c) 2013.
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -10,10 +10,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
