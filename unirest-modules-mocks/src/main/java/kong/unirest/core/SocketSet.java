10801885b24dbcba2f68ea53d065b859fdee1ae0
ryber <Ryan.Bergman@gmail.com>
2026-05-10 16:42:34 -0500

    add javadoc for SocketSet

--- unirest-modules-mocks/src/main/java/kong/unirest/core/SocketSet.java
+++ unirest-modules-mocks/src/main/java/kong/unirest/core/SocketSet.java
@@ -28,8 +28,21 @@ package kong.unirest.core;
 import java.net.http.WebSocket;
 
 /**
- * A socket set represents a websocket and the listener for a target.
- * Each side of a websocket communication would be represented by a set
+ * Represents a pairing of a WebSocket and its associated listener for one side of a WebSocket communication.
+ * <p>
+ * A WebSocket connection involves two endpoints, each with its own socket and listener.
+ * This class encapsulates one side of that communication, bundling together the socket
+ * instance, its listener, and a descriptive name for identification purposes.
+ * </p>
+ * <p>
+ * This is primarily used in the mocking framework to simulate WebSocket connections
+ * for testing purposes.
+ * </p>
+ *
+ * @param <S> the type of WebSocket, must extend {@link WebSocket}
+ * @param <L> the type of WebSocket listener, must extend {@link WebSocket.Listener}
+ * @see WebSocket
+ * @see WebSocket.Listener
  */
 public class SocketSet<S extends WebSocket, L extends WebSocket.Listener> {
 
@@ -37,29 +50,63 @@ public class SocketSet<S extends WebSocket, L extends WebSocket.Listener> {
     private final L listener;
     private final String name;
 
+    /**
+     * Creates a new SocketSet with the specified socket, listener, and name.
+     *
+     * @param socket   the WebSocket instance for this side of the connection
+     * @param listener the listener that will receive WebSocket events
+     * @param name     a descriptive name for this socket set, used for identification and in {@link #toString()}
+     */
     public SocketSet(S socket, L listener, String name){
         this.socket = socket;
         this.listener = listener;
         this.name = name;
     }
 
+    /**
+     * Returns the WebSocket instance for this socket set.
+     *
+     * @return the WebSocket instance
+     */
     public S getSocket() {
         return socket;
     }
 
+    /**
+     * Returns the WebSocket listener for this socket set.
+     *
+     * @return the WebSocket listener
+     */
     public L getListener() {
         return listener;
     }
 
+    /**
+     * Returns the descriptive name of this socket set.
+     *
+     * @return the name of this socket set
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns a string representation of this socket set.
+     *
+     * @return the name of this socket set
+     */
     @Override
     public String toString() {
         return name;
     }
 
+    /**
+     * Simulates opening the WebSocket connection by invoking the listener's {@code onOpen} callback.
+     * <p>
+     * This method triggers the {@link WebSocket.Listener#onOpen(WebSocket)} method on the
+     * associated listener, passing this socket set's WebSocket instance.
+     * </p>
+     */
     public void open() {
         listener.onOpen(socket);
     }
